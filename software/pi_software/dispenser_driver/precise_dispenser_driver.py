270518bd8f4776536b8053bd11f3fc69013d513b
wsarce <wsarcera@gmail.com>
2021-04-03 20:40:09 -0500

    Documented Python driver and added documentation to user guide
    - Added timeout to the dispense loop
    - Added documentation to Python driver functions
    - Added Python documentation to user guide

--- software/pi_software/dispenser_driver/precise_dispenser_driver.py
+++ software/pi_software/dispenser_driver/precise_dispenser_driver.py
@@ -6,7 +6,7 @@ Pi 4, a stepper motor driver HAT, mounted IR break beam, and a backplane for pow
 
 Author:     Walker Arce
 Date:       04/03/2021
-Version:    0.1
+Version:    0.2
 """
 
 import RPi.GPIO as gpio
@@ -14,39 +14,81 @@ import time
 
 
 class PreciseDispenser:
-    """A simple class definition to interact with the canine treat dispenser.
     """
-    def __init__(self):
+    A simple class definition to interact with the canine treat dispenser.
+    """
+    def __init__(self, loaded_treats=59, timeout=1):
+        """Initializer for the canine treat dispenser, configures control variables and Pi GPIO.
+
+        :param loaded_treats: The number of treats that are loaded into the dispenser for this session.  Defaults to max, 59 treats.
+        :param timeout: The amount of time before the dispenser throw an error between dispensation events
+        :returns: None.
+        """
+        # Set control variables
+        self.loaded_treats = loaded_treats
+        self.dispensing_timeout = timeout
+        self.dispensing_start = 0
+        # Setup pin definitions
+        self.TREAT = 18
         self.DIR = 22
         self.STEP = 23
-        self.TREAT = 18
+        # Configure pins
         gpio.setmode(gpio.BCM)
         gpio.setup(self.DIR, gpio.OUT)
         gpio.setup(self.STEP, gpio.OUT)
         gpio.setup(self.TREAT, gpio.IN)
+        # Set outputs and trigger events
         gpio.add_event_detect(self.TREAT, gpio.FALLING, callback=self.treat_dispensed, bouncetime=100)
         gpio.output(self.DIR, gpio.LOW)
+        # Initialize the dispensing variable to False
         self.dispensing = False
 
     def dispense_treat(self):
+        """Function to dispense a single treat.  Should not be used, use dispense_treats for error handling.
+
+        :returns: True for successful dispensation of a single treat, False for a failure to dispense the treat.
+        """
         self.dispensing = True
+        self.start = time.time()
         while self.dispensing == True:
             gpio.output(self.STEP, gpio.HIGH)
             time.sleep(0.1)
             gpio.output(self.STEP, gpio.LOW)
             time.sleep(0.1)
+            if time.time() - self.start > self.dispensing_timeout:
+                return False
         return True
 
     def dispense_treats(self, num_treats):
+        """Dispenses the specified number of treats.  If the requested number of treats is greater than the remaining treats, a ValueError is thrown.  If the treat dispenser
+        loop times out, a ValueError is thrown.
+
+        :param num_treats: A number between 1 and the remaining number of treats.
+        :returns: None.  Throws ValueError when an error event is encountered.
+        """
+        if num_treats > self.loaded_treats:
+            raise ValueError("Not enough treats remaining to dispense {0} treats!".format(num_treats))
         while num_treats is not 0:
             if (self.dispense_treat()):
                 num_treats -= 1
-                print("Remaining Treats: {0}".format(num_treats))
+                self.loaded_treats -= 1
+            else:
+                raise ValueError("Treat was unable to dispense, check for jams!")
 
     def treat_dispensed(self, channel):
+        """A callback function for when a treat is dispensed.  This is detected on the falling edge of the IR break beam.
+
+        :param channel: The channel to identify the callback source.
+        :returns: None.
+        """
+        self.start = time.time()
         self.dispensing = False
 
     def close(self):
+        """Closes the dispenser object and cleans up the GPIO assignments.
+
+        :returns: None.
+        """
         gpio.cleanup()
 
     
