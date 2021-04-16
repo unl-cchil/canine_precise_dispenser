807e4f1d7347364846b9dcc998ed82ae77bd7dbc
wsarce <wsarcera@gmail.com>
2021-04-16 13:15:30 -0500

    Performed reliability testing and logged it to spreadsheet, added test script

---
+++ software/dispenser_test.py
@@ -0,0 +1,12 @@
+from precise_dispenser_driver import PreciseDispenser
+import time
+
+dispenser = PreciseDispenser(59, timeout=3)
+
+for i in range(1, 11):
+    try:
+        dispenser.dispense_treats(i)
+        print("Dispensed {0} treats".format(i))
+    except ValueError as e:
+        print(e)
+    time.sleep(3)
